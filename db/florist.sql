50f66da5de18f054626798df62ad685652117200
Rikke Blom <78023071+rikkeblom@users.noreply.github.com>
2023-05-04 13:59:38 +0200

    database-connection changes

--- db/florist.sql
+++ db/florist.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `florist`
 --
+CREATE DATABASE IF NOT EXISTS `florist` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `florist`;
 
 -- --------------------------------------------------------
 
