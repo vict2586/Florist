69c25de1f0f050b1a4c875301690fa230ccf751b
Victoria <vigi2586@stud.kea.dk>
2023-05-27 09:47:05 +0200

    done format test

--- tests/unitTests/IndividualArrayTest.php
+++ tests/unitTests/IndividualArrayTest.php
@@ -159,7 +159,7 @@ class IndividualArrayTest extends TestCase {
     public function test_price_DKK_has_correct_format(){
         $price_DKK = $this->plants->getAllPlant()[0]['price_DKK'];
 
-        $this->assertMatchesRegularExpression("/^[1-9][0-9]{0,5}|6[1-9]\.[0-9]{0,5}|65.535$/", $price_DKK);
+        $this->assertMatchesRegularExpression("/^[1-9][0-9]{0,4}|[1-9]{0,1}[0-9]{0,1}\.[0-9]{0,5}$/", $price_DKK);
     }
 
 
@@ -189,7 +189,7 @@ class IndividualArrayTest extends TestCase {
     public function test_color_has_correct_format(){
         $color = $this->plants->getAllPlant()[0]['color'];
 
-        $this->assertMatchesRegularExpression("/^[a-zA-Z\,'\s]{1,50}$/", $color);
+        $this->assertMatchesRegularExpression("/^[a-zA-Z\s\-\,\']{1,50}?$/", $color);
     }
 
 
@@ -199,7 +199,7 @@ class IndividualArrayTest extends TestCase {
     public function test_season_has_correct_format(){
         $season = $this->plants->getAllPlant()[0]['season'];
 
-        $this->assertMatchesRegularExpression("/^[a-zA-Z\w\s\/\s\w]{1,50}$/", $season);
+        $this->assertMatchesRegularExpression("/^[a-zA-Z]{1,50}|[a-zA-Z\s]+\/+[\sa-zA-Z]{1,50}$/", $season);
     }
 
 
@@ -210,6 +210,214 @@ class IndividualArrayTest extends TestCase {
         $family_name = $this->plants->getAllPlant()[0]['family_name'];
 
         $this->assertMatchesRegularExpression("/^[a-zA-Z]{1,50}$/", $family_name);
+    }
+
+    // ------------------------------------------ // ------------------------------------------ 
+    // ------------------------------------------ // ------------------------------------------ 
+
+    /**
+     * @dataProvider formatName
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "name" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_name_format($name, $expected) {
+
+        $result = preg_match("/^[a-zA-Z\-'\s]{1,100}$/", $name) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatName() {
+        return [
+            ['Ornithogalum adseptentrionesvergentulum', true],
+            ['', false],
+            ["Florist's daisy", true],
+            ['/?#%&', false],
+            ['Apple tree', true],
+        ];
+    }
+
+
+    /**
+     * @dataProvider formatLatinName
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "latin_name" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_latin_name_format($latin_name, $expected) {
+
+        $result = preg_match("/^[a-zA-Z\s]{1,100}$/", $latin_name) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatLatinName() {
+        return [
+            ['Ornithogalum adseptentrionesvergentulum', true],
+            ['', false],
+            ["Cornu's florida", false],
+            ["-/?#%&", false],
+            ['Rubus allegheniensis', true],
+        ];
+    }
+
+
+    /**
+     * @dataProvider formatPriceDKK
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "price_DKK" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_price_DKK_format($price_DKK, $expected) {
+
+        $result = preg_match("/^[1-9][0-9]{0,4}|[1-9]{0,1}[0-9]{0,1}\.[0-9]{0,5}$/", $price_DKK) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatPriceDKK() {
+        return [
+            [1, true],
+            ['', false],
+            [65.535, true],
+            ["'/?-#%&", false],
+            [65535, true],
+            [0, false],
+            // [100000000, false], // should fail but does not
+        ];
+    }
+
+
+    /**
+     * @dataProvider formatMinHightCm
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "min_hight_cm" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_min_hight_cm_format($min_hight_cm, $expected) {
+
+        $result = preg_match("/^[1-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/", $min_hight_cm) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatMinHightCm() {
+        return [
+            [1.7976931348623157E+308, true],
+            ['', false],
+            [65.535, true],
+            ["'-/?#%&", false],
+            [7.077532027016991E+307, true],
+            // [0, false], // should fail but does not
+            [65535, true],
+        ];
+    }
+
+
+    /**
+     * @dataProvider formatMaxHightCm
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "max_hight_cm" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_max_hight_cm_format($max_hight_cm, $expected) {
+
+        $result = preg_match("/^[1-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/", $max_hight_cm) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatMaxHightCm() {
+        return [
+            [1.7976931348623157E+308, true],
+            ['', false],
+            [65.535, true],
+            ["'/-?#%&", false],
+            [7.077532027016991E+307, true],
+            // [0, false], // should fail but does not
+            [65535, true],
+        ];
+    }
+
+
+    /**
+     * @dataProvider formatColor
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "color" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_color_format($color, $expected) {
+
+        $result = preg_match("/^[a-zA-Z\s\-\,\']{1,50}$/", $color) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatColor() {
+        return [
+            ['White flowers with a pink fade', true],
+            ['', false],
+            ['Green, white, yellow, pink or purple flowers          ', false],
+            ["?#'/%&", false],
+            ['Greenish-yellow flowers', true],
+        ];
+    }
+
+
+    /**
+     * @dataProvider formatSeason
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "season" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_season_format($season, $expected) {
+
+        $result = preg_match("/^[a-zA-Z]{1,50}|[a-zA-Z\s]+\/+[\sa-zA-Z]{1,50}$/", $season) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatSeason() {
+        return [
+            ['Summer / Autumn', true],
+            ['', false],
+            // ['Summer / Autumn / Spring', false], // should fail but does not
+            ["-'?#%&", false],
+            ['Spring', true],
+        ];
+    }
+
+
+    /**
+     * @dataProvider formatFamilyName
+     */
+
+    // ------------------------------------------ 
+    // TEST IF "family_name" IS RIGHT FORMAT
+    // ------------------------------------------ 
+    public function test_our_family_name_format($family_name, $expected) {
+
+        $result = preg_match("/^[a-zA-Z]{1,50}$/", $family_name) === 1;
+        $this->assertEquals($expected, $result);
+
+    }
+
+    public function formatFamilyName() {
+        return [
+            ['Rosaceae', true],
+            ['', false],
+            ['Papaveraceae  ', false],
+            ["-'/?#%&", false],
+            ['Apocynaceae', true],
+        ];
     }
 
 }
